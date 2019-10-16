5d7b9a6f89dee8d6d239781b26114a1a6a05743f
AntonioManuel9 <antonio_manuel12@hotmail.es>
2019-10-16 14:54:19 +0200

    Fase2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from  '@angular/common/http';
+import { map }  from  'rxjs/operators';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+ 
+  coches: any;
+  
+  constructor(private httpClient: HttpClient) {
+    this.coches = this.httpClient.get('https://api.myjson.com/bins/aknmy').pipe(map(res => res['results']));
+  }
+ 
 }
+
