510be016ecb7dad52c52eb71d816a841da85d260
AntonioManuel9 <antonio_manuel12@hotmail.es>
2019-12-10 12:58:38 +0100

    Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,8 @@ import { map }  from  'rxjs/operators';
 export class HomePage {
  
   coches: any;
+  cabeceraProyecto: string = "Aplicación Personal de vehículos";
+  buscar: string = '';
   
   constructor(private httpClient: HttpClient) {
     this.coches = this.httpClient.get('https://api.myjson.com/bins/18i0bg').pipe(map(res => res['results']));
